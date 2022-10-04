3599aa6a6ee51d0e33eb856568edcdff3c931248
doingen <ee_v_chiacky@yahoo.co.jp>
2022-10-05 00:26:02 +0900

    second_searchメソッド修正、カレンダー表示

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -11,43 +11,66 @@ use Carbon\Carbon;
 class ReserveController extends Controller
 {
     public function index(){
-        $selected = 0;
+        $selected_a = 0;
         $aircraft = Aircraft::all();
 
-        return view('reserve', ['selected' => $selected, 'aircraft' => $aircraft]);
+        return view('reserve', compact('selected_a', 'aircraft'));
     }
 
     public function first_search(Request $request){
-        $selected = $request->aircraft_id;
+        $selected_a = $request->aircraft_id;
+        $selected_s = 0;
         $aircraft = Aircraft::all();
         $spots = Aircraft::where('id', $request->aircraft_id)
                         ->with('spots')
                         ->get();
-        return view('reserve', ['selected' => $selected, 'aircraft' => $aircraft, 'spots' => $spots]);
+        return view('reserve', compact('selected_a', 'selected_s', 'aircraft', 'spots'));
     }
 
     public function second_search(Request $request, $aircraft_id){
-        $reserved = Reservation::where('spot_id', $request->spot_id)->get();
-        
-        
-        $timeArray = [];
+        $start_hour = 7;
+        $end_hour = 22;
+        $open_days = 14;
+
+        $aircraft = Aircraft::all();
+        $selected_a = $aircraft_id;
+        $selected_s = $request->spot_id;
+        $spots = Aircraft::where('id', $aircraft_id)
+                        ->with('spots')
+                        ->get();
+        $reserved = Reservation::where('spot_id', $selected_s)->get();
+
         $dt = new Carbon;
-        for($i=0; $i<=14; $i++){
-            $start_time = $dt->today()->addDay($i)->addHour(7);
-            $end_time = $dt->today()->addDay($i)->addHour(22);
+        $calender_array = [];
+        for($i=$start_hour; $i<$end_hour; $i++) {
+            for($j=0; $j<=45; $j=$j+15){
+                for($k=0; $k<$open_days; $k++){
+                    $td = $dt->today()->addDay($k)->addHour($i)->addMinutes($j);
+                    $calender_array[] = $td;
+                }
+            }
+        }
+        $last_key = array_key_last($calender_array);
+        
+        $time_array = [];
+        for($i=0; $i<$open_days; $i++){
+            $start_time = $dt->today()->addDay($i)->addHour($start_hour);
+            $end_time = $dt->today()->addDay($i)->addHour($end_hour);
             $period = CarbonPeriod::create($start_time, $end_time)->minute(15);
-            $timeArray[] = $period;
+            $time_array[] = $period;
         }
 
         $arr = [];
-        foreach($timeArray as $time){
+        foreach($time_array as $time){
             foreach($time as $time){
                 $date = $time->format('Y-m-d H:i:s');
                 $result = $reserved->where('start_at','<=',$date)->where('end_at','>',$date);
                 if($result->isNotEmpty()){
-                    $arr[$date] = 1;
+                    $arr[] = $date;
                 }
             }
         }
+
+        return view('reserve', compact('open_days', 'aircraft', 'selected_a', 'selected_s', 'spots', 'calender_array','last_key', 'arr'));
     }
 }
--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -10,26 +10,49 @@
         @csrf
         <select name="aircraft_id">
           @foreach($aircraft as $aircraft)
-            <option value="{{$aircraft->id}}" @if($selected == $aircraft->id) selected @endif>{{$aircraft->name}}</option>
+            <option value="{{$aircraft->id}}" @if($selected_a == $aircraft->id) selected @endif>{{$aircraft->name}}</option>
           @endforeach
         </select>
         <button>決定</button>
       </form>
-      <form method="get" action="{{route('reserve.second_search', ['aircraft_id' => $selected])}}">
-        {{$selected}}
+      <form method="get" action="{{route('reserve.second_search', ['aircraft_id' => $selected_a])}}">
         @csrf
         <p>&#9313;駐機スポットを選択し検索ボタンを押してください</p>
         <select name="spot_id">
           @isset($spots)
             @foreach($spots as $spots)
               @foreach($spots->spots as $spot)
-                <option value="{{$spot->id}}">{{$spot->name}}</option>
+                <option value="{{$spot->id}}" @if($selected_s == $spot->id) selected @endif>{{$spot->name}}</option>
               @endforeach
             @endforeach
           @endisset
         </select>
         <button>検索</button>
       </form>
+      @isset($calender_array)
+      <div class="reserve__calender">
+        <table>
+          <tr>
+            <th></th>
+            @for($i=0; $i<$open_days; $i++)
+              <th>{{$calender_array[$i]->format("m/d")}}</th>
+            @endfor
+          </tr>
+          @for($i=0; $i<=$last_key; $i=$i+$open_days)
+            <tr>
+              <td>{{$calender_array[$i]->format("H:i")}}</td>
+              @for($j=$i; $j<$i+$open_days; $j++)
+                @if(in_array($calender_array[$j], $arr)) 
+                  <td>✕</td>
+                @else
+                  <td>◎</td>
+                @endif
+              @endfor
+            </tr>
+          @endfor
+        </table>
+      </div>
+      @endisset
     </div>
   </div>
 </main>
