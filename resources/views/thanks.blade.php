55126d6bfcf2002a8fd61862bff1965d850da8a2
doingen <ee_v_chiacky@yahoo.co.jp>
2022-10-29 15:10:30 +0900

    thanks.blade分岐作成

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -3,9 +3,15 @@
 @section('contents')
 <div class="thanks__wrapper">
   <div class="thanks__contents">
+    @if(Request::routeIs('reserve.create'))
     <p>ご予約ありがとうございます</p><br>
     <a href="{{route('main.index')}}" class="thanks__contents--top">トップへ</a>
     <a href="" class="thanks__contents--mypage">マイページ</a>
+    @elseif(Request::routeIs('register.create'))
+    <p>ご登録ありがとうございます。</p>
+    <p>スポット予約の前に、メール認証をお願いいたします。</p>
+    <a href="{{route('main.index')}}" class="thanks__contents--top">トップへ</a>
+    @endif
   </div>
 </div>
 @endsection
